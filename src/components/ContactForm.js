7a57e8d1162486649417c77f86904803be9570aa
Blake Fletcher <blkfltchr@gmail.com>
2020-08-07 16:11:53 -0400

    Added back contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,38 +1,285 @@
 import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
 import PropTypes from 'prop-types'
 import styled from '@emotion/styled'
-import PageBody from '../components/PageBody'
 
-const ContactPage = styled.div`
+/*
+  ⚠️ This is an example of a contact form powered with Netlify form handling.
+  Be sure to review the Netlify documentation for more information:
+  https://www.netlify.com/docs/form-handling/
+*/
+
+const Form = styled.form`
   max-width: ${props => props.theme.sizes.maxWidthCentered};
   margin: 0 auto;
   display: flex;
-  flex-flow: column;
+  flex-flow: row wrap;
   justify-content: space-between;
   align-items: flex-start;
+  input,
+  textarea {
+    font-family: inherit;
+    font-size: inherit;
+    background: ${props => props.theme.colors.tertiary};
+    color: ${props => props.theme.colors.primary};
+    border-radius: 2px;
+    padding: 1em;
+    &::-webkit-input-placeholder {
+      color: gray;
+    }
+    &::-moz-placeholder {
+      color: gray;
+    }
+    &:-ms-input-placeholder {
+      color: gray;
+    }
+    &:-moz-placeholder {
+      color: gray;
+    }
+    &:required {
+      box-shadow: none;
+    }
+  }
+  &::before {
+    content: '';
+    background: black;
+    height: 100%;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    transition: 0.2s all;
+    opacity: ${props => (props.overlay ? '.8' : '0')};
+    visibility: ${props => (props.overlay ? 'visible' : 'hidden')};
+  }
+  p {
+    line-height: 1.25;
+    margin: 0 0 2em 0;
+    span {
+      font-weight: 700;
+    }
+  }
+  a {
+    color: ${props => props.theme.colors.accent};
+    font-weight: 800;
+  }
+  input {
+    &:last-of-type {
+      background: ${props => props.theme.colors.accent};
+      padding: 12px 25px;
+      margin-bottom: 20px;
+      font-weight: bold;
+      color: white !important;
+      border-radius: none;
+      text-transform: uppercase;
+    }
+  }
 `
-const ContactForm = () => {
-  const { contentfulContactInfo } = useStaticQuery(
-    graphql`
-      query ContactQuery {
-        contentfulContactInfo(id: {eq: "db75b2a8-9609-5863-b23e-29095d41d6e3"}) {
-          body {
-            childMarkdownRemark {
-              html
-              excerpt(pruneLength: 320)
-            }
-          }
-        }
-      }
-    `
-  )
-  const { body } = contentfulContactInfo
-  return (
-    <ContactPage>
-      <PageBody body={body} />
-    </ContactPage>
-  )
+
+const Name = styled.input`
+  margin: 0 0 1em 0;
+  width: 100%;
+  @media (min-width: ${props => props.theme.responsive.small}) {
+    width: 49%;
+  }
+`
+
+const Email = styled.input`
+  margin: 0 0 1em 0;
+  width: 100%;
+  @media (min-width: ${props => props.theme.responsive.small}) {
+    width: 49%;
+  }
+`
+
+const Message = styled.textarea`
+  width: 100%;
+  margin: 0 0 1em 0;
+  line-height: 1.6;
+  min-height: 250px;
+  resize: vertical;
+`
+
+const Submit = styled.input`
+  background: ${props => props.theme.colors.accent} !important;
+  color: black !important;
+  cursor: pointer;
+  transition: 0.2s;
+  padding: 12px 25px;
+  text-align: center;
+  border-radius: 10px;
+`
+
+const Modal = styled.div`
+  background: white;
+  color: black;
+  padding: 2em;
+  border-radius: 2px;
+  position: fixed;
+  min-width: 75%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  margin: 0 auto;
+  z-index: 99;
+  display: flex;
+  flex-flow: column;
+  align-items: flex-start;
+  transition: 0.2s all;
+  opacity: ${props => (props.visible ? '1' : '0')};
+  visibility: ${props => (props.visible ? 'visible' : 'hidden')};
+  @media screen and (min-width: ${props => props.theme.responsive.small}) {
+    min-width: inherit;
+    max-width: 400px;
+  }
+  p {
+    line-height: 1.6;
+    margin: 0 0 2em 0;
+  }
+`
+
+const Button = styled.div`
+  background: ${props => props.theme.colors.accent} !important;
+  font-size: 1em;
+  display: inline-block;
+  margin: 0 auto;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  color: white;
+  padding: 1em;
+  border-radius: 2px;
+  text-decoration: none;
+  transition: 0.2s;
+  z-index: 99;
+  &:focus {
+    outline: none;
+  }
+`
+
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
+
+class ContactForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      email: '',
+      message: '',
+      showModal: false,
+    }
+  }
+
+  handleInputChange = event => {
+    const target = event.target
+    const value = target.value
+    const name = target.name
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  handleSubmit = event => {
+    fetch('/?no-cache=1', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...this.state }),
+    })
+      .then(this.handleSuccess)
+      .catch(error => alert(error))
+    event.preventDefault()
+  }
+
+  handleSuccess = () => {
+    this.setState({
+      name: '',
+      email: '',
+      message: '',
+      showModal: true,
+    })
+  }
+
+  closeModal = () => {
+    this.setState({ showModal: false })
+  }
+
+  render() {
+    return (
+      <Form
+        name="contact"
+        onSubmit={this.handleSubmit}
+        data-netlify="true"
+        data-netlify-honeypot="bot"
+        overlay={this.state.showModal}
+        onClick={this.closeModal}
+      >
+        <p>
+          Located on the east side of Yonge street in between Keewatin and Erskine, Salon Afif is steps away from the Yonge and Eglinton subway station. Easy to find by car or foot, make sure to book an appointment today.
+        </p>
+        <p>
+          At Salon Afif we give all our new customers a free consultation to discuss their hair look wishes and our recommendations to ensure a positive experience from the first try.
+        </p>
+        <p style={{ width: '100%' }}>
+          <span>HOURS</span><br />
+          Monday: 10am-7pm<br />
+          Tuesday: 9:30am-7pm<br />
+          Wednesday: 9:30am-7pm<br />
+          Thursday: 9:30am-7pm<br />
+          Friday: 9:30am-7pm<br />
+          Saturday: 9am-5pm<br />
+          Sunday: Closed
+        </p>
+        <p>
+          To contact us, please call us at <a href="tel:416-488-2161">416-488-2161</a> or email us at <a href="mailto:salonafif@hotmail.com">salonafif@hotmail.com</a>.
+        </p>
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
+          <label>
+            Don’t fill this out:{' '}
+            <input name="bot" onChange={this.handleInputChange} />
+          </label>
+        </p>
+
+        <Name
+          name="name"
+          type="text"
+          placeholder="Full Name"
+          value={this.state.name}
+          onChange={this.handleInputChange}
+          required
+        />
+        <Email
+          name="email"
+          type="email"
+          placeholder="Email"
+          value={this.state.email}
+          onChange={this.handleInputChange}
+          required
+        />
+        <Message
+          name="message"
+          type="text"
+          placeholder="Message"
+          value={this.state.message}
+          onChange={this.handleInputChange}
+          required
+        />
+        <Submit name="submit" type="submit" value="Send" />
+
+        <Modal visible={this.state.showModal}>
+          <p>
+            Thank you for reaching out. We will get back to you as soon as
+            possible.
+          </p>
+          <Button onClick={this.closeModal}>Okay</Button>
+        </Modal>
+      </Form>
+    )
+  }
 }
 
 ContactForm.propTypes = {
